2e776b3597fed2056ddbb4af6c5a12821e8c799e
Cory McDonald <cory@corywmcdonald.com>
2019-01-17 18:50:57 -0600

    Art 1

--- README.md
+++ README.md
@@ -6,9 +6,17 @@ Hello there, it's time for you to explore your creative side and create some ✨
 
 That's not true
 
-# What the heck is going on here
+## What the heck is going on here
 
 We're going to be creating some artwork using Javascript and <a href="https://p5js.org/">P5.js</a>
 
 Take a look at <a href="https://p5js.org/examples/">some examples</a> of what you can do!
 
+
+First things first open up the code that's in the `sketch.js` and see what's going on there.
+
+Then experiment!
+
+## Examples 🖼
+
+![Example 1](examples/1.gip)
--- sketch.js
+++ sketch.js
@@ -2,26 +2,58 @@ function setup() {
   // Make the drawing take up the whole window
   createCanvas(windowWidth, windowHeight);
   // This creates the background color, RGB (232, 218, 239)
-  background(232, 218, 239);
+  background(white[0], white[1], white[2]);
 }
 
+// Let's define some colors here
+// https://p5js.org/learn/color.html
+let yellow = [ 242, 190, 84 ];
+let white = [ 240, 217, 207 ]
+let cyan = [ 135, 174, 180 ];
+let navy = [ 21, 62, 92 ]
+let red = [ 237, 85, 96 ];
 
-let angle = 0
+
 // This is an endless loop that acts as a frame
 function draw() {
-
-  background(232, 218, 239);
-  fill(255, 204, 0);
   noStroke();
 
-  diameter = 100
+  let points = Math.floor(random(10)) + 2;
 
-  let d1 = 50 + (sin(angle) * diameter) / 2 + diameter / 2;
-  ellipse(500, height / 2, d1, d1);
-  angle += 0.2
+  // What happens if we randomly generate these numbers?
+  let x = 50;
+  let y = 50;
+
+  // Try uncommenting these out :)
+
+  // let randomColor = random([cyan, navy, red, yellow])
+  // fill(randomColor[0], randomColor[1], randomColor[2])
+
+  star(
+    x,
+    y,
+    15, // Radius 1
+    30, // Radius 2
+    5 // 5 pointed star
+  );
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  var angle = TWO_PI / npoints;
+  var halfAngle = angle/2.0;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius2;
+    var sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a+halfAngle) * radius1;
+    sy = y + sin(a+halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  background(232, 218, 239);
+  background(white[0], white[1], white[2]);
 }
