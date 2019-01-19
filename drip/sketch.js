5cb7b285c24dec58f268b2f559fdc116e231dc60
Cory McDonald <cory@corywmcdonald.com>
2019-01-19 10:41:38 -0600

    Test

--- drip/sketch.js
+++ drip/sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 var movers = [];
 
-// Restart all the Mover objects randomly
+// Add the initial dripping objects
 function add() {
   let gap = 50;
   let margin = 10;
@@ -26,6 +26,7 @@ function draw() {
   let margin = 10;
   translate(margin * 4, margin * 4);
 
+  // This is what is moving the drops every frame
   for (var i = 0; i < movers.length; i++) {
     // Gravity is scaled by mass here!
     var gravity = createVector(0, 0.001 * movers[i].mass);
@@ -39,13 +40,14 @@ function draw() {
   // in this case 35, which is the # symbol
   let counter = 65;
 
-  // Loop as long as there is space on the canvas
+  // This is our drawing loop that we use to draw all the letters
   for (let y = 0; y < height - gap; y += gap) {
     for (let x = 0; x < width - gap; x += gap) {
       // Use the counter to retrieve individual letters by their Unicode number
       let letter = char(counter);
 
       // Add different color to the vowels and other characters
+      // Try changing this to be something different
       if (letter === "A" || letter === "R" || letter === "T") {
         fill("#ed225d");
       } else {
@@ -56,6 +58,7 @@ function draw() {
       text(letter, x, y);
 
       counter++;
+      // If we go over the set of ASCII characters then we shoudl go back to the start :)
       if (counter > 124) {
         counter = 65;
       }
@@ -65,13 +68,14 @@ function draw() {
 
 class Drip {
   constructor(x, y) {
-    this.mass = 0.3;
+    this.mass = 0.1;
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
   }
 
   applyForce(force) {
+    // Create a vector of x,y so that we can apply force
     var f = p5.Vector.div(force, this.mass);
     this.acceleration.add(f);
   }
@@ -81,6 +85,9 @@ class Drip {
     this.velocity.add(this.acceleration);
     // position changes by velocity
     this.position.add(this.velocity);
+    // Add in a random X value to seem like it's moving back and forth
+    this.position.x = this.position.x + random(-0.5, 0.5);
+
     // We must clear acceleration each frame
     this.acceleration.mult(0);
   }
